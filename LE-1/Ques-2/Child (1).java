6560ad6ac5daec145f4f596f1429c3433e9181c2
Priyanshi Mishra <118006559+24Priyanshi@users.noreply.github.com>
2025-01-23 15:26:22 +0530

    Add files via upload

---
+++ LE-1/Ques-2/Child (1).java
@@ -0,0 +1,7 @@
+class Child extends Mother {
+    int x;
+    void show(){
+        System.out.println("Child class called");
+    }
+    
+}
---
+++ LE-1/Ques-3/Child (2).java
@@ -0,0 +1,7 @@
+class Child extends Mother {
+    int x;
+    static void show(){
+        System.out.println("Child class called");
+    }
+    
+}
