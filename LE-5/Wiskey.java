78e1647710f26364f7bc56d38e1f73b7ccb33681
Priyanshi Mishra <118006559+24Priyanshi@users.noreply.github.com>
2025-03-27 14:17:41 +0530

    Add files via upload

---
+++ LE-5/Wiskey.java
@@ -0,0 +1,10 @@
+class Wiskey extends Beverage{
+    protected void addCondiment(){
+        System.out.println("Add some ice");
+        
+    }
+    protected void stir(){
+        System.out.println("Stir for 30 seconds");
+        
+    }
+}
